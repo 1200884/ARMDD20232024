03ec1069ff645900fb0515ec3371cdac5fd73f91
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-01-01 19:14:54 +0000

    Criação dos lookups

---
+++ Folder/StagingArea/CreateTableFinishedGoodsFlagDescription_Lookup.sql
@@ -0,0 +1,10 @@
+IF NOT EXISTS(SELECT name FROM sys.tables WHERE name = 'FinishedGoodsFlagDescription_Lookup')
+BEGIN
+	CREATE TABLE [dbo].[FinishedGoodsFlagDescription_Lookup](
+		[FinishedGoodsFlag] [char](1) NULL,
+		[FinishedGoodsFlagDescription] [nvarchar](10) NOT NULL
+	) ON [PRIMARY]
+	INSERT [dbo].[FinishedGoodsFlagDescription_Lookup] ([FinishedGoodsFlag], [FinishedGoodsFlagDescription]) VALUES (NULL, N'Unknown')
+	INSERT [dbo].[FinishedGoodsFlagDescription_Lookup] ([FinishedGoodsFlag], [FinishedGoodsFlagDescription]) VALUES (N'1', N'Yes')
+	INSERT [dbo].[FinishedGoodsFlagDescription_Lookup] ([FinishedGoodsFlag], [FinishedGoodsFlagDescription]) VALUES (N'0', N'No')
+END
