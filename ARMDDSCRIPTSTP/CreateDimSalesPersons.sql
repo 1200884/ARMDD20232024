21417c178506fd657dd77be51dd7c4b288d510c7
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-01-04 03:49:20 +0000

    .

--- ARMDDSCRIPTSTP/CreateDimSalesPersons.sql
+++ ARMDDSCRIPTSTP/CreateDimSalesPersons.sql
@@ -6,15 +6,15 @@ IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'DimSalesPersons')
 		[Bonus] [money] NOT NULL,
 		[ComissionPct] [smallmoney] NOT NULL,
 		[SalesYTD] [money] NOT NULL,
-		[SalesLastYear] [int] NOT NULL,
+		[SalesLastYear] [money] NOT NULL,
 		[NationalIDNumber] [nvarchar] (15) NOT NULL,
 		[LoginID] [nvarchar] (256) NOT NULL,
 		[JobTitle] [nvarchar] (50) NOT NULL,
 		[BirthDate] [date] NOT NULL,
-		[MaritalStatus] [nchar] (1) NOT NULL,
-		[Gender] [nchar] (1) NOT NULL,
+		[MaritalStatus] [nchar] (10) NOT NULL,
+		[Gender] [nchar] (6) NOT NULL,
 		[HireDate] [date] NOT NULL,
-		[SalariedFlag] [bit] NOT NULL,
+		[SalariedFlag] [nvarchar] (3) NOT NULL,
 		[VacationHours] [smallint] NOT NULL,
 		[SickLeaveHours] [smallint] NOT NULL,
 		[PersonType] [nvarchar] (20) NOT NULL,
@@ -27,7 +27,7 @@ IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'DimSalesPersons')
 		[EffectiveDate] [datetime] NOT NULL,
 		[ExpiredDate] [datetime] NULL,
 		[Is_Current] [nvarchar] (3) NOT NULL
-		CONSTRAINT [PK_DimStore] PRIMARY KEY CLUSTERED 
+		CONSTRAINT [PK_SalesPersonKey] PRIMARY KEY CLUSTERED 
 	(
 		[SalesPersonKey] ASC
 	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
