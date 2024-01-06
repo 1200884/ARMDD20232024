75f0c2cfa66e5ed1eb0a4427add7f584e3603bf3
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-01-06 15:31:02 +0000

    .

--- Folder/StagingArea/CreateStageTableCustomersDQP.sql
+++ Folder/StagingArea/CreateStageTableCustomersDQP.sql
@@ -1,12 +1,12 @@
 IF NOT EXISTS (SELECT name from sys.tables WHERE name = 'CustomersDQP')
 	CREATE TABLE [dbo].[CustomersDQP](
 		[CustomerID] [int] NOT NULL,
-		[PersonType] [nvarchar](20) NOT NULL,
+		[PersonType] [nvarchar](20) NULL,
 		[Title] [nvarchar](8) NULL,
-		[FirstName] [nvarchar](50) NOT NULL,
+		[FirstName] [nvarchar](50) NULL,
 		[MiddleName] [nvarchar](50) NULL,
-		[LastName] [nvarchar](50) NOT NULL,
-		[CreatedDate] [date] NOT NULL,
+		[LastName] [nvarchar](50) NULL,
+		[CreatedDate] [date] NULL,
 		[ModifiedDate] [date] NULL,
         DQP nvarchar(100)
 	)
